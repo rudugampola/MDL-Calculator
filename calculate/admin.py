332a021123d9401fd004c594a00d01dd2957cab5
rudugampola <ravindu365@gmail.com>
2023-02-28 22:27:10 -0800

    Saving file data to database works

--- calculate/admin.py
+++ calculate/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Analyte
+from .models import Data
+
+admin.site.register(Data)
 
 
-class AnalyteAdmin(admin.ModelAdmin):
-    model = Analyte
-    list_display = ("analyte_name", "cas", "mdl_cal_spike", "mdl_cal_blank",
-                    "mdl_verified", "current_mdl", "current_rl", "units")
+class DataAdmin(admin.ModelAdmin):
+    list_display = ('file_name', 'analyte_name', 'cmp', 'cust_sample_id', 'lab_sample_id', 'matrix', 'matrix_desc', 'method_desc', 'result', 'result_units', 'run_date', 'sample_type', 'analyst', 'app_high', 'app_low', 'collection_site', 'collect_date', 'cust_id', 'cust_name', 'cust_workorder_id', 'dilution', 'formatted_idl', 'formatted_pql',
+                    'formatted_rdl', 'formatted_result', 'hsn', 'idl', 'lab_workorder_id', 'location_seq', 'pql', 'prep_analyst', 'prep_run_date', 'prep_schedule_seq', 'profile_name', 'project_seq', 'rdl', 'receive_date', 'reqnbr', 'result_id', 'result_pos', 'sample_status', 'sample_type_desc', 'schedule_seq', 'text_result')
--- calculate/views.py
+++ calculate/views.py
@@ -5,7 +5,7 @@ from django.contrib import messages
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 
-# from .models import Analyte
+from .models import Data
 
 
 class bcolors:
@@ -29,6 +29,8 @@ def calculate_mdl(excel_file):
 
     # group by analyte_name
     groups = mdl_data.groupby('analyte_name')
+    filtered_reps = df[df['sample_type'] == 'MDLREP']
+    filtered_blks = df[df['sample_type'].isin(['MDLBLK', 'MB'])]
 
     # calculate MDLs and MDLb for each analyte
     results = {}
@@ -97,7 +99,15 @@ def calculate_mdl(excel_file):
     date_from = mdl_data['run_date'].min()
     date_to = mdl_data['run_date'].max()
 
-    results["date"] = (date_from, date_to)
+    # Check if any field is empty, if so, set it to None
+    for col in filtered_reps.select_dtypes(include=['object']):
+        filtered_reps[col] = filtered_reps[col].apply(
+            lambda x: x if x != '' else None)
+    for col in filtered_blks.select_dtypes(include=['object']):
+        filtered_blks[col] = filtered_blks[col].apply(
+            lambda x: x if x != '' else None)
+
+    results["metadata"] = (date_from, date_to, filtered_reps, filtered_blks)
     return results
 
 
@@ -110,22 +120,137 @@ def import_excel(request):
             return HttpResponseRedirect(reverse("calculate:import_excel"))
 
         excel_file = request.FILES['excel_file']
+        file_name = excel_file.name
 
         if not excel_file.name.endswith('.xlsx'):
             messages.error(request, 'File is not an Excel file!')
             return HttpResponseRedirect(reverse("calculate:import_excel"))
 
         print(bcolors.WARNING + "Importing Excel: " +
-              excel_file.name + bcolors.ENDC)
+              file_name + bcolors.ENDC)
 
         mdl_result = calculate_mdl(excel_file)
         # Get date_from and date_to
-        date_from = mdl_result["date"][0]
-        date_to = mdl_result["date"][1]
+        date_from = mdl_result["metadata"][0]
+        date_to = mdl_result["metadata"][1]
+
+        # Save REPS and BLKS data to Database
+        reps = mdl_result["metadata"][2]
+        blks = mdl_result["metadata"][3]
+
+        # Save REPS and BLKS data to Database
+        rep_count = 0
+        blk_count = 0
+        for index, row in reps.iterrows():
+            rep = Data(
+                file_name=file_name,
+                analyte_name=row['analyte_name'],
+                cmp=row['cmp'],
+                cust_sample_id=row['cust_sample_id'],
+                lab_sample_id=row['lab_sample_id'],
+                matrix=row['matrix'],
+                matrix_desc=row['matrix_desc'],
+                method_desc=row['method_desc'],
+                result=row['result'],
+                result_units=row['result_units'],
+                run_date=row['run_date'],
+                sample_type=row['sample_type'],
+                analyst=row['analyst'],
+                app_high=row['app_high'],
+                app_low=row['app_low'],
+                collection_site=row['collection_site'],
+                collect_date=row['collect_date'],
+                cust_id=row['cust_id'],
+                cust_name=row['cust_name'],
+                cust_workorder_id=row['cust_workorder_id'],
+                dilution=row['dilution'],
+                formatted_idl=row['formatted_idl'],
+                formatted_pql=row['formatted_pql'],
+                formatted_rdl=row['formatted_rdl'],
+                formatted_result=row['formatted_result'],
+                hsn=row['hsn'],
+                idl=row['idl'],
+                lab_workorder_id=row['lab_workorder_id'],
+                location_seq=row['location_seq'],
+                pql=row['pql'],
+                prep_analyst=row['prep_analyst'],
+                prep_run_date=row['prep_run_date'],
+                prep_schedule_seq=row['prep_schedule_seq'],
+                profile_name=row['profile_name'],
+                project_seq=row['project_seq'],
+                rdl=row['rdl'],
+                receive_date=row['receive_date'],
+                reqnbr=row['reqnbr'],
+                result_id=row['result_id'],
+                result_pos=row['result_pos'],
+                sample_status=row['sample_status'],
+                sample_type_desc=row['sample_type_desc'],
+                schedule_seq=row['schedule_seq'],
+                text_result=row['text_result'],
+            )
+            rep_count += 1
+            # print(bcolors.WARNING + str(rep) + bcolors.ENDC)
+            rep.save()
+
+        for index, row in blks.iterrows():
+            blk = Data(
+                file_name=file_name,
+                analyte_name=row['analyte_name'],
+                cmp=row['cmp'],
+                cust_sample_id=row['cust_sample_id'],
+                lab_sample_id=row['lab_sample_id'],
+                matrix=row['matrix'],
+                matrix_desc=row['matrix_desc'],
+                method_desc=row['method_desc'],
+                result=row['result'],
+                result_units=row['result_units'],
+                run_date=row['run_date'],
+                sample_type=row['sample_type'],
+                analyst=row['analyst'],
+                app_high=row['app_high'],
+                app_low=row['app_low'],
+                collection_site=row['collection_site'],
+                collect_date=row['collect_date'],
+                cust_id=row['cust_id'],
+                cust_name=row['cust_name'],
+                cust_workorder_id=row['cust_workorder_id'],
+                dilution=row['dilution'],
+                formatted_idl=row['formatted_idl'],
+                formatted_pql=row['formatted_pql'],
+                formatted_rdl=row['formatted_rdl'],
+                formatted_result=row['formatted_result'],
+                hsn=row['hsn'],
+                idl=row['idl'],
+                lab_workorder_id=row['lab_workorder_id'],
+                location_seq=row['location_seq'],
+                pql=row['pql'],
+                prep_analyst=row['prep_analyst'],
+                prep_run_date=row['prep_run_date'],
+                prep_schedule_seq=row['prep_schedule_seq'],
+                profile_name=row['profile_name'],
+                project_seq=row['project_seq'],
+                rdl=row['rdl'],
+                receive_date=row['receive_date'],
+                reqnbr=row['reqnbr'],
+                result_id=row['result_id'],
+                result_pos=row['result_pos'],
+                sample_status=row['sample_status'],
+                sample_type_desc=row['sample_type_desc'],
+                schedule_seq=row['schedule_seq'],
+                text_result=row['text_result'],
+            )
+            blk_count += 1
+            # print(bcolors.WARNING + str(blk) + bcolors.ENDC)
+            blk.save()
+
+        print(bcolors.OKGREEN + "Saved: " +
+              str(rep_count) + " MDL Replicate Data Points." + bcolors.ENDC)
+        print(bcolors.OKGREEN + "Saved: " +
+              str(blk_count) + " MDL Blank Data Points." + bcolors.ENDC)
 
         # Remove "date" from mdl_result
-        mdl_result.pop("date", None)
+        mdl_result.pop("metadata", None)
 
-        return render(request, 'calculate/mdl_result.html', {'mdl_results': mdl_result, 'date_from': date_from, 'date_to': date_to, 'title': 'Calculate MDL'})
+        return render(request, 'calculate/mdl_result.html', {'mdl_results': mdl_result, 'date_from': date_from, 'date_to': date_to, 'title': 'Calculate MDL', 'file_name': file_name})
     else:  # GET request
         return render(request, 'calculate/import_excel.html', {'title': 'Import MDL Data'})
